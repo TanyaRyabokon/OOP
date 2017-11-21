4c4bb7c6735210ecad23d7a3294f53ec1d7612ed
Tanya Ryabokon <reebokon@gmail.com>
2017-11-21 21:37:55 +0200

    lab3 delegate

--- lab3/Lab1_OOP/Program.cs
+++ lab3/Lab1_OOP/Program.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Lab1_OOP
 {
     class Program
     {
+        delegate void del(AstronomicalBody i);
         static void Main(string[] args)
         {
             try
@@ -23,10 +24,12 @@ namespace Lab1_OOP
                 AstronomicalBody ceres = new AstronomicalBody("Ceres", 3345.4F, 789.4F, 347.83F, 7.3600000000000000F);
                 AstronomicalBody fortuna = new AstronomicalBody("Fortuna", 795.67F, 41.3F, 78.4F, 9.3400000000000000F);
 
+                del Add = x => system.Add(x);
+
                 //system forming 
-                system.Add(astBody);
-                system.Add(ceres);
-                system.Add(fortuna);
+                Add(astBody);
+                Add(ceres);
+                Add(fortuna);
 
                 //show the new system
                 Console.WriteLine("New System forms\n");
@@ -36,23 +39,28 @@ namespace Lab1_OOP
                     Console.WriteLine("{0}\n", system[i]);
                     i++;
                 }
-                
-           
+
+                Thread t1 = new Thread
+                  (delegate ()
+                  {
+                      //save to file 
+                      Console.WriteLine("\n\nSave to file\n\n");
+                      Serialization fileManager = new Serialization();
+                      fileManager.WriteToFileFromClass(system.Bodies, @"D:\КПИ\ООП\OOP\lab3\Lab1_OOP\data\out.json");
+                  });
+                t1.Start();
                 //space ship
                 SpaceShip ship = new SpaceShip("Millenium falcon", 23.5645F, 19945645.45F, astBody);
 
-                Console.WriteLine("Ship {0} started from {1}\n", ship.Name, ship.Start.Name);
+                Console.WriteLine("\nShip {0} started from {1}\n", ship.Name, ship.Start.Name);
 
                 //asteroid belt
                 AsteroidBelt sp = new AsteroidBelt(ship);
 
+                Thread.Sleep(2000);
+
                 Console.WriteLine("Ship hit the asteroid belt\n");
                 ship.Hit();
-
-                //save to file 
-                Console.WriteLine("\n\nSave to file\n\n");
-                Serialization fileManager = new Serialization();
-                fileManager.WriteToFileFromClass(system.Bodies, @"D:\КПИ\ООП\OOP\lab3\Lab1_OOP\data\out.json");
 
                 Console.ReadKey();
             }
